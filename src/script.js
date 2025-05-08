e48f448bfb28256b6d2b6129c8dd9d5325bc86ad
PxMach <moundjidmachghour58@gmail.com>
2025-05-08 21:50:36 +0100

    Ajout d'une fonctionnalité de compteur de produits dans le panier, mise à jour de l'affichage du panier vide et amélioration du style CSS pour le conteneur du panier et les éléments associés. Modification de l'icône du panier pour un meilleur contraste.

--- src/script.js
+++ src/script.js
@@ -1,32 +1,71 @@
 const form = document.querySelector("form");
 const plus = document.querySelector(".plus");
 const minus = document.querySelector(".minus");
-const num = document.querySelector(".num");
+const cartEmpty = document.querySelector(".cart-empty ");
+const numElements = document.querySelectorAll(".num");
+const cartCount = document.querySelector(".cart-count");
 
 const iconCart = document.getElementById("icon-cart");
 const cartContainer = document.querySelector(".cart-container");
+const addition = document.querySelector(".addition");
+const cartContent = document.querySelector(".cart-content");
+const addCart = document.getElementById("add-cart");
 
 let count = 0;
+let add = 0;
 
+// Click the button + or - for the number of product
 plus.addEventListener("click", function () {
    count++;
-   num.textContent = count;
+   numElements.forEach((num) => {
+      num.textContent = count;
+   });
+   add = 125 * count;
+   addition.textContent = `$${add}`;
+   updateCartCount();
 });
 
 minus.addEventListener("click", function () {
    if (count > 0) {
       count--;
    }
-   num.textContent = count;
+   numElements.forEach((num) => {
+      num.textContent = count;
+   });
+   add = 125 * count;
+   addition.textContent = `$${add}`;
+   updateCartCount();
 });
 
+function updateCartCount() {
+   if (count > 0) {
+      cartCount.style.display = "block";
+      cartCount.textContent = count;
+   } else {
+      cartCount.style.display = "none";
+   }
+}
+
+// for display your cart
 iconCart.addEventListener("click", function () {
    if (
       cartContainer.style.display === "none" ||
       cartContainer.style.display === ""
    ) {
       cartContainer.style.display = "block";
-   } else {
+   } else if (count === 0 || cartContainer.style.display === "block") {
       cartContainer.style.display = "none";
+   }
+});
+
+// for add the product in the cart
+addCart.addEventListener("click", function (e) {
+   e.preventDefault(); // Empêche la soumission de la page
+   if (count === 0) {
+      cartContent.style.display = "none";
+      cartEmpty.style.display ="block"
+   } else {
+      cartContent.style.display = "block";
+      cartEmpty.style.display = "none";
    }
 });
